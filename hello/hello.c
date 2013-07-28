23dabaa933e63a8cead7ee95c1661f9106883e6f
psachin <iclcoolster@gmail.com>
2013-07-28 23:15:21 +0530

    Subject: add explaination for __init and __exit macros
    Description:

--- hello/hello.c
+++ hello/hello.c
@@ -1,16 +1,21 @@
 #include<linux/init.h>		/* to initiate a module */
 #include<linux/module.h>	/* to recognise this module as a mmodule */
 
-
-static int hello_init(void) {
+/* The __init macro causes the init function to be discarded and its
+   memory to be freed once the init function finishes for built-in
+   drivers, but not for loadable modules */
+static int __init hello_init(void) {
   /* entry point of the module */
-  printk(KERN_ALERT "Test: hello cruel world\n");
+  printk(KERN_INFO "Test: hello cruel world\n");
   return 0;
 }
 
-static void hello_exit(void) {
+/* the __exit macro works the same way as __init, it causes the
+   omission of the function when built into the kernel but not for
+   loadable modules */
+static void __exit hello_exit(void) {
   /* exit point of the module */
-  printk(KERN_ALERT "Test: GoodBye kernel\n");
+  printk(KERN_INFO "Test: GoodBye kernel\n");
   
 }
 

c12517dc1b31be1d3efde6555e989b4af426dfc9
psachin <iclcoolster@gmail.com>
2014-04-16 13:27:03 +0530

    Subject: More verbose messages.
    Description:

--- usb/pen-driver.c
+++ usb/pen-driver.c
@@ -10,16 +10,41 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
+
+static struct usb_device *device;
  
 static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-  printk(KERN_INFO "[PD]: device (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
+  struct usb_host_interface *iface_desc;
+  struct usb_endpoint_descriptor *endpoint;
+  int i;
+
+  iface_desc = interface->cur_altsetting;
+  printk(KERN_INFO "[PD]: device i/f %d, (%04X:%04X) plugged\n", 
+	 iface_desc->desc.bInterfaceNumber, id->idVendor, id->idProduct);
+  printk(KERN_INFO "ID->bNumEndpoints: %02X\n", 
+	 iface_desc->desc.bNumEndpoints);
+  printk(KERN_INFO "ID->bInterfaceClass: %02X\n",
+	 iface_desc->desc.bInterfaceClass);
+
+  for (i = 0; i < iface_desc->desc.bNumEndpoints; i++)
+    {
+      endpoint = &iface_desc->endpoint[i].desc;
+      
+      printk(KERN_INFO "ED[%d]->bEndpointAddress: 0x%02X\n",
+	     i, endpoint->bEndpointAddress);
+      printk(KERN_INFO "ED[%d]->bmAttributes: 0x%02X\n",
+	     i, endpoint->bmAttributes);
+      printk(KERN_INFO "ED[%d]->wMaxPacketSize: 0x%04X (%d)\n",
+	     i, endpoint->wMaxPacketSize, endpoint->wMaxPacketSize);
+    }
+  //  device = interface_to_usbdev(interface);
   return 0;
 }
  
 static void pen_disconnect(struct usb_interface *interface)
 {
-  printk(KERN_INFO "[PD]: device removed\n");
+  printk(KERN_INFO "[PD]: device disconnected\n");
 }
  
 static struct usb_device_id pen_table[] =
